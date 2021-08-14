0044f1ce24c03445c06279e5ff3aaac1e1543fb5
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-08-14 23:04:14 +0530

    question answer pages java

--- app/src/main/java/com/example/seprojectsemester5/QuestionAnswerPage.java
+++ app/src/main/java/com/example/seprojectsemester5/QuestionAnswerPage.java
@@ -3,6 +3,8 @@ package com.example.seprojectsemester5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class QuestionAnswerPage extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class QuestionAnswerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_answer_page);
+        RadioGroup fever = (RadioGroup) findViewById(R.id.fever);
+        RadioGroup cough = (RadioGroup) findViewById(R.id.cough);
+        RadioGroup bodyPain = (RadioGroup) findViewById(R.id.bodyPain);
+        RadioGroup breathingProblem = (RadioGroup) findViewById(R.id.breathingProblem);
+        RadioGroup eyeCheck = (RadioGroup) findViewById(R.id.eyeCheck);
+        RadioGroup skinCheck = (RadioGroup) findViewById(R.id.skinCheck);
+        EditText edit_text = (EditText) findViewById(R.id.edit_text);
     }
 }

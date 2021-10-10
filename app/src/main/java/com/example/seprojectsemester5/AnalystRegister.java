1e48d5487bfe8df001f29d0c1756070b43d48b36
vijayjoshi16 <joshivijay324@gmail.com>
2021-10-10 23:43:15 +0530

    Added Role Selection Screen

--- app/src/main/java/com/example/seprojectsemester5/AnalystRegister.java
+++ app/src/main/java/com/example/seprojectsemester5/AnalystRegister.java
@@ -2,13 +2,24 @@ package com.example.seprojectsemester5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AnalystRegister extends AppCompatActivity {
-
+    TextView goToAnalystLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyst_register);
+        goToAnalystLogin = findViewById(R.id.goToAnalystLogin);
+        goToAnalystLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AnalystRegister.this, AnalystLogin.class));
+                finish();
+            }
+        });
     }
 }

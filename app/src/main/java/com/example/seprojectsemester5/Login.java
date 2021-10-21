9c7f33d088a0fcd1cc0a4ff08174d6c4a9effafd
Prerit Kumar Jha <preritkrjha@gmail.com>
2021-10-21 15:18:46 +0530

    Login Working

--- app/src/main/java/com/example/seprojectsemester5/Login.java
+++ app/src/main/java/com/example/seprojectsemester5/Login.java
@@ -2,10 +2,30 @@ package com.example.seprojectsemester5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
     private TextView GoToRegister;
@@ -21,5 +41,63 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this,Register.class));
             }
         });
+
+
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        Button Login = findViewById(R.id.loginSubmit);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String url = "https://g0uravlathwal.herokuapp.com/user/login";
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("email", email.getText().toString());
+                params.put("password", password.getText().toString());
+                params.put("role", "Collector");
+                final RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                JsonObjectRequest req = new JsonObjectRequest(url, new JSONObject(params),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    VolleyLog.v("Response:%n %s", response.toString(4));
+                                    Log.e("VOLLEY", response.toString());
+                                    Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_SHORT).show();
+                                    if(response.toString().substring(11,12).equals("O")){
+                                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+                                        SharedPreferences sharedpreferences = getSharedPreferences("FIXED", Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                                        editor.putString("jwtToken", response.toString().substring(24, response.toString().length() - 2));
+                                        editor.commit();
+                                        Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                                        startActivity(intent);
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.e("Error: ", error.getMessage());
+                        Log.e("VOLLEY", error.toString());
+                        Toast.makeText(getApplicationContext(), "Failed " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        return params;
+                    }
+                };
+                requestQueue.add(req);
+            }
+        });
+
     }
 }
